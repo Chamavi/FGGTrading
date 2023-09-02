eedd1f8039260c5a39f118baa77a2a08358d436d
Chamavi <familyggtrading@gmail.com>
2023-09-02 06:27:47 +0200

    aez

--- src/js/auth/login.js
+++ src/js/auth/login.js
@@ -1,34 +1,28 @@
 import { Database } from "bun:sqlite";
 
 export async function login(json) {
-
   try {
-
     let username = json["username"];
     let password1 = json["password"];
-    console.log(1)
+    console.log(1);
+    const db = new Database("./src/db/mydb.sqlite");
     const test = db
-          .query(`select username, password from login where username = $username `)
-          .get({ $username: username }) 
-          console.log(2)
-    if (
-          test !== null
-      ) {
-        let hash = test["password"]
-        const isMatch = await Bun.password.verify(password1, hash);
-        if(isMatch){
-            return "true"
-        }else{
-            return "mot de passe incorrect"
-        }
-        
-      }else{
-        return "username ou mail inconnu"
+      .query(`select username, password from login where username = $username `)
+      .get({ $username: username });
+    console.log(json);
+    if (test !== null) {
+      let hash = test["password"];
+      const isMatch = await Bun.password.verify(password1[0], hash);
+      if (isMatch) {
+        return "true";
+      } else {
+        return "mot de passe incorrect";
       }
-    
-    return "aze" 
+    } else {
+      return "username ou mail inconnu";
+    }
+
   } catch (error) {
-    return "Veuilleze reesayer ou contacter le site";
+    return "Veuillee reesayer ou contacter le site";
   }
 }
-
